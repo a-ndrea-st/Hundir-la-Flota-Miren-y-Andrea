a30ad270501c8cb0d6f6307a23cc0c8c268b4a77
a-ndrea-st <a.sancho.torrico@gmail.com>
2023-12-19 23:22:07 +0100

    Definición de más funciones

--- Hundir_la_Flota_Miren_Andrea.py
+++ Hundir_la_Flota_Miren_Andrea.py
@@ -53,48 +53,65 @@ tablero_vista_para_maquina_de_jugadorx=np.full((10, 10)," ")     #en este se ve 
 Jugadorx va modificando el tablero de la máquina y el de su vista
 La máquina va modificando el tablero de jugadorx y el de su vista
 
+#ÍNDICE
+
+def seguir_jugando():
+    mensaje = """
+        Recuerda que puedes:
+        Insertar 1 para salir del juego
+        Insertar 2 para ver tu tablero
+        Insertar 3 para ver tu vista del tablero de la máquina (comprobar impactos)
+        """
+    print(mensaje)
+    opcion=int(input("Inserta opción: "))
+    while opcion != 1:
+        if opcion ==2:
+            print(tablero_jugadorx)
+        if opcion ==3:
+            print(tablero_vista_para_jugadorx_de_maquina)
+    print("¡Hasta luego!")
 
 #FUNCIONES PARA CADA TURNO:
 def turno_jugadorx(tablero_maquina):
-    if turno_jugadorx:
-        coordenada1= input("Dime un numero",)
-        coordenada1= input("Dime un numero",)
-        disparo_jugadorx (coordenada1,coordenada2)
+    seguir_jugando()
+    coordenada1= input("Introduce un numero de fila para tu coordenada",)
+    coordenada2= input("Introduce un numero de columna para tu coordenada",)
+    disparo_jugadorx (coordenada1,coordenada2,tablero_maquina)
 
 def turno_maquina(tablero_jugadorx):
-    if turno_maquina:
-        coordenada1 = np.random.randint(0, 10)
-        coordenada2 = np.random.randint(0, 10)
-        disparo_maquina (coordenada1,coordenada2)
+    coordenada1 = np.random.randint(0, 10)
+    coordenada2 = np.random.randint(0, 10)
+    disparo_maquina (coordenada1,coordenada2,tablero_jugadorx)
 
 #FIN DE LA PARTIDA   (se puede hacer todo con un bucle while)
     
 def fin_partida(tablero_jugadorx, tablero_maquina):
     if "O" not in tablero_jugadorx :
         print("¡Perdiste!")
+        return True
     elif "O" not in tablero_maquina:   #Jugadorx gana cuando todos los "O" se han convertido en "X" en el tablero de la maquina
-    print("¡Ganaste, humanx!")
-    continue
+        print("¡Ganaste, humanx!")
+        return True
+    return False
+ 
+#########BUCLE PRINCIPAL ###################
+tablero_maquina = np.full((10, 10), " ")
+colocar_barcos(tablero_maquina, [1, 1, 1, 1, 2, 2, 2, 3, 3, 4])
 
-
-#BUCLE PRINCIPAL:
+tablero_jugadorx = np.full((10, 10), " ")
+colocar_barcos(tablero_jugadorx, [1, 1, 1, 1, 2, 2, 2, 3, 3, 4])
 
 while "O" in tablero_jugadorx or "O" in tablero_maquina:  
-
     turno_jugadorx(tablero_maquina)
-
     if fin_partida(tablero_jugadorx, tablero_maquina):
         break
- 
     turno_maquina(tablero_jugadorx)
-
-    if verificar_ganador(tablero_jugadorx, tablero_maquina)
+    if fin_partida(tablero_jugadorx, tablero_maquina)
         break
 
-"O" not in tablero_jugadorx:
+if "O" not in tablero_jugadorx:
     print("¡Perdiste!")
-
-"O" not in tablero_maquina:   #Jugadorx gana cuando todos los "O" se han convertido en "X" en el tablero de la maquina
+if "O" not in tablero_maquina:   #Jugadorx gana cuando todos los "O" se han convertido en "X" en el tablero de la maquina
     print("¡Ganaste!")
 
 
@@ -104,53 +121,60 @@ while "O" in tablero_jugadorx or "O" in tablero_maquina:
 #Como jugadorx solamente veo mi tablero y la vista del tablero de la máquina con X y -
 
 #Disparo de persona
-def disparo_jugadorx (coordenada1,coordenada2):
+def disparo_jugadorx (coordenada1, coordenada2, tablero_maquina, tablero_vista_para_jugadorx_de_maquina):
     if tablero_maquina[coordenada1,coordenada2]== " ":
         tablero_maquina[coordenada1,coordenada2]= "_"
         tablero_vista_para_jugadorx_de_maquina=[coordenada1,coordenada2]= "_"
         print("¡Agua!")
         print(tablero_vista_para_jugadorx_de_maquina)
-        break
-    if tablero_maquina[coordenada1,coordenada2]== "O":
+    elif tablero_maquina[coordenada1,coordenada2]== "O":
         tablero_maquina[coordenada1,coordenada2]= "X"
         tablero_vista_para_jugadorx_de_maquina[coordenada1,coordenada2]= "X"
         print("¡Has tocado un barco!")
         print(tablero_vista_para_jugadorx_de_maquina)
-        continue
-    if tablero_maquina[coordenada1,coordenada2]== "O" and ------ #algo!
-        tablero_maquina[coordenada1,coordenada2]= "X"
-        tablero_vista_para_jugadorx_de_maquina[coordenada1,coordenada2]= "X"
-        print("¡Has hundido un barco!")
-        barcos_hundidos_demaquina= barcos_hundidos_demaquina+1
-        print(tablero_vista_para_jugadorx_de_maquina)
-        continue
+        if barco_hundido(coordenada1, coordenada2, tablero_maquina):
+            print("¡Has hundido un barco!")
+
+         '''
+        if tablero_maquina[coordenada1,coordenada2]== "O" and ------ #algo!
+            tablero_maquina[coordenada1,coordenada2]= "X"
+            tablero_vista_para_jugadorx_de_maquina[coordenada1,coordenada2]= "X"
+            print("¡Has hundido un barco!")
+            barcos_hundidos_demaquina= barcos_hundidos_demaquina+1
+            print(tablero_vista_para_jugadorx_de_maquina)
+          '''
 
 #Disparo de la máquina
-def disparo_maquina (coordenada1,coordenada2):
-    if tablero_maquina[coordenada1,coordenada2]== " ":
-        tablero_maquina[coordenada1,coordenada2]= "_"
+def disparo_maquina (coordenada1,coordenada2, tablero_jugadorx, tablero_vista_para_maquina_de_jugadorx):
+    if tablero_jugadorx[coordenada1,coordenada2]== " ":
+        tablero_jugadorx[coordenada1,coordenada2]= "_"
         tablero_vista_para_maquina_de_jugadorx[coordenada1,coordenada2]= "_"
         print("¡Agua!")
-        break
-    if tablero_maquina[coordenada1,coordenada2]== "O":
-        tablero_maquina[coordenada1,coordenada2]= "X"
+    elif tablero_jugadorx[coordenada1,coordenada2]== "O":
+        tablero_jugadorx[coordenada1,coordenada2]= "X"
         tablero_vista_para_maquina_de_jugadorx[coordenada1,coordenada2]= "X"
         print("¡La máquina te ha tocado un barco!")
-        continue
-    if tablero_maquina[coordenada1,coordenada2]== "O" and ------ #algo!
-        tablero_maquina[coordenada1,coordenada2]= "X"
-        tablero_vista_para_maquina_de_jugadorx[coordenada1,coordenada2]= "X"
-        print("¡La máquina te ha hundido un barco!")
-        barcos_hundidos_dejugadorx= barcos_hundidos_dejugadorx+1
-        continue
+        if barco_hundido(coordenada1, coordenada2, tablero_jugadorx):
+            print("¡La máquina te ha hundido un barco!")
+
+        '''
+        if tablero_maquina[coordenada1,coordenada2]== "O" and ------ #algo!
+            tablero_maquina[coordenada1,coordenada2]= "X"
+            tablero_vista_para_maquina_de_jugadorx[coordenada1,coordenada2]= "X"
+            print("¡La máquina te ha hundido un barco!")
+            barcos_hundidos_dejugadorx= barcos_hundidos_dejugadorx+1
+        '''
 
 #HUNDIR UN BARCO
-Ver cómo hacer para que de hundido.
+def barco_hundido(coordenada1, coordenada2, tablero):
+    dicc_esloras = {4: 1, 3: 2, 2: 3, 1: 4}  
+    for x, y in dicc_esloras.items():
+        for i in range(y):
 
-
-#BUCLE DE DISPAROS
-if tablero[coordenada1,coordenada2]= "X":
-    disparo(coordenada1,coordenada2)
+            for i in range(x):
+                if all(tablero[coordenada1, coordenada2 + i] == "X"):
+                    return True 
+    return False 
 
 
 
